7a49861b35395039d8eede0d7dc8f419634ddf40
MatrixCrawler <matrixcrawler@matrixcrawler.net>
2016-06-18 22:06:59 +0200

    * initial commit

---
+++ src/main/groovy/grails/plugin/springsecurity/oauth2/google/GoogleOauth2SpringToken.groovy
@@ -0,0 +1,39 @@
+package grails.plugin.springsecurity.oauth2.google
+
+import com.github.scribejava.core.model.OAuth2AccessToken
+import grails.plugin.springsecurity.oauth2.token.OAuth2SpringToken
+
+/**
+ * Always code as if the guy who ends up maintaining your code
+ * will be a violent psychopath that knows where you live.
+ *
+ * - John Woods
+ *
+ * Created on 18.06.2016
+ * @author MatrixCrawler
+ */
+class GoogleOauth2SpringToken  extends OAuth2SpringToken{
+
+    OAuth2GoogleService oAuth2GoogleService
+    private String email
+
+    GoogleOauth2SpringToken(OAuth2AccessToken accessToken, String email) {
+        super(accessToken)
+        this.email = email
+    }
+
+    @Override
+    String getProviderName() {
+        return oAuth2GoogleService.providerID
+    }
+
+    @Override
+    String getSocialId() {
+        return email
+    }
+
+    @Override
+    String getScreenName() {
+        return email
+    }
+}
